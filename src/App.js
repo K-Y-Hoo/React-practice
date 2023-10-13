a06a67d2d5ed86f259d679913b17c991c4ea5133
K-Y-Hoo <bookopen1@gmail.com>
2023-10-13 14:43:59 +0900

    feat: 버튼 클릭 시 Alert 생성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import {useState} from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 
 
 const App = () => {
   const [charge, setCharge] = useState("");
   const [amount, setAmount] = useState(0);
+
+  const [alert, setAlert] = useState({show: false});
 
   const handleCharge = (e) => {
     setCharge(e.target.value);
@@ -22,10 +25,17 @@ const App = () => {
     {id: 3, charge: "식비", amount: 1200},
   ])
 
+  const handleAlert = ({type, text}) => {
+    setAlert({show: true, type, text});
+    setTimeout(() => {
+      setAlert({show: false});
+    }, 3000);
+  }
 
   const handleDelete = (id) => {
     const newExpenses = expenses.filter(expense => expense.id !== id);
     setExpenses(newExpenses);
+    handleAlert({type: "danger", text: "아이템이 삭제되었습니다."})
   }
 
   const handleSubmit = (e) => {
@@ -36,13 +46,15 @@ const App = () => {
       setExpenses(newExpenses);
       setCharge("");
       setAmount(0);
+      handleAlert({type: "success", text: "아이템이 생성되었습니다."})
     } else {
-      console.log('error');
+      handleAlert({type: "danger", text: "charge는 빈 값일 수 없으면 amount는 0보다 커야 합니다."})
     }
   }
 
   return(
     <main className="main-container">
+      {alert.show ? <Alert type={alert.type} text={alert.text} /> : null}
       <h1>예산 계산기</h1>
 
       <div style={{ width:'100%', backgroundColor: 'white', padding: '1rem'}}>
